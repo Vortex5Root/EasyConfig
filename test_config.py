cbbe5d8be07e9f0be368d09739aaaff1b16286ea
vortex <vortex@vortex.lan>
2024-08-17 09:34:07 -0500

    Tests Updated!

--- test_config.py
+++ test_config.py
@@ -11,14 +11,13 @@ def test_start_config_manager():
     config_manager = Config_Manager('test_configs')
     assert config_manager.main_path.exists()
     assert config_manager.main_path.is_dir()
-    assert config_manager.configs == {}
 
 def test_new_config():
     config_manager = Config_Manager('test_configs')
     config = config_manager.new_config('test_config')
-    assert config_manager.configs == {'test_config': config}
+    assert 'test_config' in config_manager.configs
     assert config.path == config_manager.main_path
-    assert config.config_name == 'test_config.json'
+    assert config.config_name == 'test_config'
 
 def test_new_config_sub_directory():
     config_manager = Config_Manager('test_configs')
@@ -26,31 +25,39 @@ def test_new_config_sub_directory():
     assert "sub_directory/test_config" in config_manager.configs
     assert config_manager.configs["sub_directory/test_config"] == config
     assert config.path == config_manager.main_path / 'sub_directory'
-    assert config.config_name == 'test_config.json'
+    assert config.config_name == 'test_config'
 
 def test_save_configs():
     config_manager = Config_Manager('test_configs')
     config = config_manager.new_config('test_config')
     config.test = 'test'
+    config.test2 = 'test2'
     config.save_config()
     config_manager.save_configs()
     config_manager.load_configs()
     assert "test_config" in config_manager.configs
-    assert config_manager.configs["test_config"] == config
-    assert (config_manager.configs["test_config"].path / config_manager.configs["test_config"].config_name).exists()
+    assert config_manager.configs["test_config"].__dict__ == config.__dict__
+    assert (config_manager.configs["test_config"].path / (config_manager.configs["test_config"].config_name + ".json")).exists()
     assert config_manager.configs["test_config"].test == 'test'
+
+def test_update_config():
+    config_manager = Config_Manager('test_configs')
+    config_manager.load_configs()
+    config = config_manager.configs["test_config"]
+    config.test3 = 'test3'
+    config.save_config()
+    config_manager.save_configs()
+    assert "test_config" in config_manager.configs
+    assert config_manager.configs["test_config"].__dict__ == config.__dict__
+    assert (config_manager.configs["test_config"].path / (config_manager.configs["test_config"].config_name + ".json")).exists()
+    assert config_manager.configs["test_config"].test3 == 'test3'
 
 def test_load_configs():
     config_manager = Config_Manager('test_configs')
-    config = config_manager.new_config('test_config')
-    config.test = 'test'
-    config.save_config()
     config_manager.load_configs()
     assert "test_config" in config_manager.configs
-    assert config_manager.configs["test_config"] == config
-    assert (config_manager.configs["test_config"].path / config_manager.configs["test_config"].config_name).exists()
+    assert config_manager.configs["test_config"].__dict__ != {}
+    assert (config_manager.configs["test_config"].path / (config_manager.configs["test_config"].config_name + ".json")).exists()
     assert config_manager.configs["test_config"].test == 'test'
-
-def test_remove_configs():
-    shutil.rmtree('test_configs')
-    assert not os.path.exists('test_configs')
+    assert config_manager.configs["test_config"].test2 == 'test2'
+    assert config_manager.configs["test_config"].test3 == 'test3'
