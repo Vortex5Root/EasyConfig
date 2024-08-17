51d6ead6c6618fc3a40acf79389041a8ea0a7808
vortex <vortex@vortex.lan>
2024-08-17 09:33:52 -0500

    Object File Name fixed!

--- easyconfig/Objects/__init__.py
+++ easyconfig/Objects/__init__.py
@@ -12,22 +12,28 @@ class Config:
     def __init__(self, path: Path, config_name: str):
         self.path = path
         if config_name.endswith('.json'):
-            self.config_name = config_name
+            self.config_name = config_name[:-5]
         else:
-            self.config_name = config_name + '.json'
+            self.config_name = config_name
     
     def load_config(self):
-        info = json.loads(open(self.path / self.config_name, 'r').read())
+        config_name = self.config_name
+        if not config_name.endswith('.json'):
+            config_name += '.json'
+        info = json.loads(open(self.path / config_name, 'r').read())
         for key in info:
             setattr(self, key, info[key])
         
     def save_config(self):
         info = {}
+        config_name = self.config_name
+        if not config_name.endswith('.json'):
+            config_name += '.json'
         for key in self.__dict__:
             print(key)
             if key not in ['path', 'config_name']:
                 info[key] = getattr(self, key)
-        with open(self.path / self.config_name, 'w') as f:
+        with open(self.path / config_name, 'w') as f:
             f.write(json.dumps(info, indent=4))
 
     def remove_config(self):
